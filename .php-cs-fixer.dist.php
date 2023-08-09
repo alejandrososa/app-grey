c8f3ae997fdaa2407fc80c041ed4bd7b15d3fc85
Alejandro Sosa <alesjohnson@hotmail.com>
2023-08-09 12:56:37 +0200

    FEATURE: Coding standard tools

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,13 +1,37 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
+use PhpCsFixer\Finder;
+use PhpCsFixer\Config;
+$finder = (new Finder())
     ->in(__DIR__)
     ->exclude('var')
+    ->exclude('.git/')
+    ->exclude('.temp/')
+    ->exclude('.tools/')
+    ->exclude('.vscode/')
+    ->exclude('.idea/')
 ;
 
-return (new PhpCsFixer\Config())
+return (new Config())
     ->setRules([
         '@Symfony' => true,
+        '@PSR12' => true,
+        '@PHP81Migration' => true,
+        '@PHP80Migration:risky' => true,    // this also needs: ->setRiskyAllowed(true)
+
+        // override some @PHPxxMigration rules, because CG supports only php v7.3
+        'assign_null_coalescing_to_coalesce_equal' => false, // override @PHP81Migration, ??= requires php v7.4
+        'use_arrow_functions' => false,     // override @PHP80Migration:risky, => fn() requires php v7.4
+        'modernize_strpos' => false,        // override @PHP80Migration:risky, str_contains() requires php v8.0
+
+        // override some @Symfony rules
+        'binary_operator_spaces' => ['operators' => ['=' => null, '=>' => null, 'and' => null, 'or' => null]],
+        'blank_line_before_statement' => false,
+        'class_attributes_separation' => false,
+        'concat_space' => ['spacing' => 'one'],
+        'increment_style'=> ['style' => 'post'],
+        'phpdoc_to_comment' => false,
+        'yoda_style' => false,
     ])
     ->setFinder($finder)
-;
+    ;
--- rector.php
+++ rector.php
@@ -1,37 +1,69 @@
 <?php
 
+declare(strict_types=1);
+
 use Rector\Config\RectorConfig;
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\PHPUnit\Set\PHPUnitLevelSetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\SetList;
+use Rector\Symfony\Set\SensiolabsSetList;
+use Rector\Symfony\Set\SymfonySetList;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 
 return static function (RectorConfig $rectorConfig): void {
     // get parameters
     $parameters = $rectorConfig->parameters();
-    $parameters->set(Option::PATHS, [
-        __DIR__.'/src',
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
     ]);
+    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
+
+    // basic rules
+    $rectorConfig->importNames();
+    $rectorConfig->importShortClasses();
+
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_82);
 
     // register single rule
     $rectorConfig->rule(TypedPropertyFromAssignsRector::class);
     $rectorConfig->rule(TypedPropertyFromStrictConstructorRector::class);
 
+    $rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
+
     // here we can define, what sets of rules will be applied
     // tip: use "SetList" class to autocomplete sets with your IDE
     $rectorConfig->sets([
+        SetList::PHP_82,
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
     ]);
 
-    $rectorConfig->sets([SetList::PHP_82]);
-    $rectorConfig->paths([
-        __DIR__.'/src',
-        __DIR__.'/tests',
+    // symfony rules
+    $rectorConfig->sets([
+        SymfonySetList::SYMFONY_63,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+        SensiolabsSetList::ANNOTATIONS_TO_ATTRIBUTES,
     ]);
 
-    // $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
+    // doctrine rules
+    $rectorConfig->sets([
+        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        DoctrineSetList::DOCTRINE_CODE_QUALITY,
+    ]);
+
+    // phpunit rules
+    $rectorConfig->sets([
+        PHPUnitSetList::PHPUNIT_100,
+        PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+    ]);
+
     $rectorConfig->indent(' ', 4);
 };
